8942a39bb837427a88d7eb046e68d25ef716039c
akhil <akhilsails@gmail.com>
2022-10-21 10:24:34 +0530

    working stage-2 code

--- main.go
+++ main.go
@@ -25,9 +25,25 @@ func main() {
 	app.Get("/", func(ctx *fiber.Ctx) {
 		ctx.Send("hello world")
 	})
+	SetupApiV1(app)
 
 	err := app.Listen(3000)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func SetupApiV1(app *fiber.App) {
+	v1 := app.Group("/v1")
+
+	SetupTodosRoutes(v1)
+}
+
+func SetupTodosRoutes(grp fiber.Router) {
+	todosRoutes := grp.Group("/todos")
+	todosRoutes.Get("/", GetTodos)
+}
+
+func GetTodos(ctx *fiber.Ctx) {
+	ctx.Status(fiber.StatusOK).JSON(todos)
 }
