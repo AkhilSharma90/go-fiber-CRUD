5ad01c85862f63007db771ebee8d30591b588b56
akhil <akhilsails@gmail.com>
2022-10-21 10:28:53 +0530

    working, stage-4 code

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/middleware"
 )
@@ -43,6 +45,7 @@ func SetupTodosRoutes(grp fiber.Router) {
 	todosRoutes := grp.Group("/todos")
 	todosRoutes.Get("/", GetTodos)
 	todosRoutes.Post("/", CreateTodo)
+	todosRoutes.Get("/:id", GetTodo)
 }
 
 func GetTodos(ctx *fiber.Ctx) {
@@ -73,4 +76,24 @@ func CreateTodo(ctx *fiber.Ctx) {
 	todos = append(todos, todo)
 
 	ctx.Status(fiber.StatusCreated).JSON(todo)
+}
+
+func GetTodo(ctx *fiber.Ctx) {
+	paramsId := ctx.Params("id")
+	id, err := strconv.Atoi(paramsId)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse id",
+		})
+		return
+	}
+
+	for _, todo := range todos {
+		if todo.Id == id {
+			ctx.Status(fiber.StatusOK).JSON(todo)
+			return
+		}
+	}
+
+	ctx.Status(fiber.StatusNotFound)
 }
